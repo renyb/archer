5e5b6474277ed0178d93be357e3abaf06335c16e
tank <tank@tank.com>
2018-06-04 14:42:14 +0800

    modify

--- archer/settings.py
+++ archer/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'hfusaf2m4ot#7)fkw#di2bu6(cv0@opwmafx5n#6=3d%x^hpl6'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+#Nginx SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
